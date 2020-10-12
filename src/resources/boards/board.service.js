3caa4edc65160c3006465cffeba3c76801156b00
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-10-12 13:27:18 +0300

    feat: implement board service

---
+++ src/resources/boards/board.service.js
@@ -0,0 +1,28 @@
+const boardsRepo = require('./board.memory.repository');
+const tasksService = require('../tasks/task.service');
+
+// GET ALL BOARDS
+const getAllBoards = () => boardsRepo.getAllBoards();
+
+// GET BOARD BY ID
+const getBoardById = id => boardsRepo.getBoardById(id);
+
+// CREATE BOARD
+const createBoard = board => boardsRepo.createBoard(board);
+
+// UPDATE BOARD
+const updateBoard = (id, board) => boardsRepo.updateBoard(id, board);
+
+// DELETE BOARD
+const deleteBoard = id => {
+  boardsRepo.deleteBoard(id);
+  tasksService.deleteAllTasks(id);
+};
+
+module.exports = {
+  getAllBoards,
+  getBoardById,
+  createBoard,
+  updateBoard,
+  deleteBoard
+};
