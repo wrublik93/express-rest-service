4b4e7ae299e4180ea1702ec6c7a5400a310d3d5b
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-10-12 19:48:57 +0300

    feat: add routers

--- src/app.js
+++ src/app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const swaggerUI = require('swagger-ui-express');
 const path = require('path');
 const YAML = require('yamljs');
+
+const boardRouter = require('./resources/boards/board.router');
 const userRouter = require('./resources/users/user.router');
+const taskRouter = require('./resources/tasks/task.router');
 
 const app = express();
 const swaggerDocument = YAML.load(path.join(__dirname, '../doc/api.yaml'));
@@ -19,6 +22,8 @@ app.use('/', (req, res, next) => {
   next();
 });
 
+app.use('/boards', boardRouter);
 app.use('/users', userRouter);
+app.use('/tasks', taskRouter);
 
 module.exports = app;
