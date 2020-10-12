99b7a0b7204b09c6c79fb45d31ccf5a1ea1be1e6
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-10-12 09:58:17 +0300

    feat: implement user service

--- src/resources/users/user.service.js
+++ src/resources/users/user.service.js
@@ -1,5 +1,28 @@
 const usersRepo = require('./user.memory.repository');
+const taskService = require('../tasks/task.service');
 
+// GET ALL USERS
 const getAll = () => usersRepo.getAll();
 
-module.exports = { getAll };
+// GET THE USER BY ID
+const getById = id => usersRepo.getById(id);
+
+// CREATE USER
+const createUser = user => usersRepo.create(user);
+
+// UPDATE USER
+const updateUser = (id, user) => usersRepo.update(id, user);
+
+// DELETE USER
+const deleteUser = id => {
+  usersRepo.deleteUser(id);
+  taskService.unassignUser(id);
+};
+
+module.exports = {
+  getAll,
+  getById,
+  createUser,
+  updateUser,
+  deleteUser
+};
