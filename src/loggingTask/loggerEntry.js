d3946b3eda7eff557c18e46efcc2479bfd8d4abd
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-10-19 16:50:29 +0300

    feat: add winston and entry point for winston

---
+++ src/loggingTask/loggerEntry.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const { createLogger, transports, format } = require('winston');
+
+module.exports = createLogger({
+  level: 'silly',
+  format: format.combine(format.colorize(), format.cli()),
+  transports: [
+    new transports.Console({
+      format: format.combine(format.colorize(), format.cli())
+    }),
+    new transports.File({
+      level: 'info',
+      filename: path.resolve(__dirname, '../../logFiles/infoLogs.log'),
+      format: format.combine(format.timestamp(), format.json())
+    }),
+    new transports.File({
+      level: 'error',
+      filename: path.resolve(__dirname, '../../logFiles/errorLogs.log'),
+      format: format.combine(format.timestamp(), format.json())
+    })
+  ]
+});
