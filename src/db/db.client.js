3b1eb258c8e1fee74425f33064f7621b2ce94a4a
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-11-07 13:33:50 +0300

    fix: change db.client, config, readme and add packages

--- src/db/db.client.js
+++ src/db/db.client.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const { MONGO_CONNECTION_STRING } = require('../common/config');
+const usersRepo = require('../resources/users/user.db.repository');
 
 const connectToDB = cb => {
   mongoose.connect(MONGO_CONNECTION_STRING, {
@@ -9,8 +10,14 @@ const connectToDB = cb => {
 
   const db = mongoose.connection;
   db.on('error', console.error.bind(console, 'connection error:'));
-  db.once('open', () => {
+  db.once('open', async () => {
     console.log('we are connected!');
+    /* await db.dropDatabase(); */
+    await usersRepo.createUser({
+      name: 'admin',
+      login: 'admin',
+      password: 'admin'
+    });
     cb();
   });
 };
